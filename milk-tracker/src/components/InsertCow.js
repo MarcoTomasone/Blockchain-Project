89de3c5f9cef0bd8b5147eb68facb6e52cc5a7d4
Stelluti Michele <m.stelluti@yahoo.com>
2023-03-30 12:50:53 +0200

    Modificato file InsertCow

--- milk-tracker/src/components/InsertCow.js
+++ milk-tracker/src/components/InsertCow.js
@@ -1,7 +1,31 @@
+import TextField from  '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import cow from '../img/cow.png';
+
 export default function InsertCow() {
-    return(
-        <div>
-          <h1>Insert Cow</h1>
-        </div>
-    );
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', width: '100%', paddingTop: '2vh', marginLeft: '-300px', overflow:'hidden'}}>
+      <h2 style={{ marginBottom: '30px', width: '50%', textAlign: 'center', color: "blue", marginLeft: "-475px" }}>Insert data cow</h2>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+      <TextField label="Cow Id"  variant="filled" borderColor="blue" borderRadius={10} focused  />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Cow Breed" variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Cow Birth"  variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Cow Residence"  variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Cow Weight"  variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <Button variant="contained" color="primary" style={{marginLeft: "-475px"}}>Send</Button>
+      <div style={{ position: 'absolute', top: 70, left: 0, width: '100vw', height: '86vh', backgroundImage: `url(${cow})`, backgroundSize: '78% 100%', backgroundRepeat: 'no-repeat', backgroundPosition: 'left center',  opacity: '0.8', zIndex: '-1', marginLeft: "300px"}}></div>
+    </div>
+  );
 }
+
+
+
