9310ca84d8fc4ddb7177b0cab63a421e6b61fb7d
Stelluti Michele <m.stelluti@yahoo.com>
2023-03-31 12:40:55 +0200

    Modificati file "InsertMilk" e "InsertProduct"

--- milk-tracker/src/components/InsertMilk.js
+++ milk-tracker/src/components/InsertMilk.js
@@ -1,8 +1,29 @@
 
+import TextField from  '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import cow from '../img/milk.png';
+
 export default function InsertMilk() {
-    return(
-        <div>
-            <h1>InsertMilk</h1>
-        </div>
-    );
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', width: '100%', paddingTop: '2vh', marginLeft: '-300px', overflow:'hidden'}}>
+      <h2 style={{ marginBottom: '30px', width: '50%', textAlign: 'center', color: "blue", marginLeft: "-475px" }}>Insert data milk</h2>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+      <TextField label="Milk Id"  variant="filled" borderColor="blue" borderRadius={10} focused  />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Cow Id" variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Date of Production"  variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Dairy Name"  variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Dairy Place"  variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <Button variant="contained" color="primary" style={{marginLeft: "-475px"}}>Send</Button>
+      <div style={{ position: 'absolute', top: 70, left: 0, width: '100vw', height: '86vh', backgroundImage: `url(${cow})`, backgroundSize: '78% 100%', backgroundRepeat: 'no-repeat', backgroundPosition: 'left center',  opacity: '0.8', zIndex: '-1', marginLeft: "300px"}}></div>
+    </div>
+  );
 }
--- milk-tracker/src/components/InsertProduct.js
+++ milk-tracker/src/components/InsertProduct.js
@@ -1,7 +1,28 @@
+import TextField from  '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import cow from '../img/products.jpg';
+
 export default function InsertProduct() {
-    return(
-        <div>
-            <h1>InsertProduct</h1>
-        </div>
-    );
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', width: '100%', paddingTop: '2vh', marginLeft: '-300px', overflow:'hidden'}}>
+      <h2 style={{ marginBottom: '30px', width: '50%', textAlign: 'center', color: "blue", marginLeft: "-475px" }}>Insert data products</h2>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+      <TextField label="Products Id"  variant="filled" borderColor="blue" borderRadius={10} focused  />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Milk Id" variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Date of production"  variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Products type"  variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <div style={{ marginBottom: '30px', width: '50%' }}>
+        <TextField label="Expiry date"  variant="filled" borderColor="blue" borderRadius={10} focused />
+      </div>
+      <Button variant="contained" color="primary" style={{marginLeft: "-475px"}}>Send</Button>
+      <div style={{ position: 'absolute', top: 70, left: 0, width: '100vw', height: '86vh', backgroundImage: `url(${cow})`, backgroundSize: '78% 100%', backgroundRepeat: 'no-repeat', backgroundPosition: 'left center',  opacity: '0.8', zIndex: '-1', marginLeft: "300px"}}></div>
+    </div>
+  );
 }
